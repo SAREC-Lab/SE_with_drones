83a9102f443f234e9e1ca7ad7975ed8c9ebcccb3
dronology-teaching@vierhauser.net <dronology-teaching@vierhauser.net>
2019-02-12 06:07:27 -0800

    Added classtest.py

---
+++ 04_multidrones/multidrone3/classtest.py
@@ -0,0 +1,16 @@
+def getboxcoordinates():
+    blackbox_lat = 86.239864
+    blackbox_lon = 41.715406
+    return blackbox_lat, blackbox_lon
+
+def getnorth():
+    return -86.241409
+
+def getsouth():
+    return -86.239242
+
+def getwest():
+    return 41.715366
+
+def geteast():
+    return 41.714373
